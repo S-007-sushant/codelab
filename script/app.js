e24793edc2aa319dbe663bb218727028d804e50f
S-007-sushant <76532272+S-007-sushant@users.noreply.github.com>
2021-10-01 00:25:55 +0530

    Updated version

--- script/app.js
+++ script/app.js
@@ -1,15 +1,23 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-    
-  });
+function navslide(){
+  let nav = document.getElementById('nav-links');
+  nav.classList.toggle("nav-active");            
 }
+
+
+function openSubject(evt,subjectName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for(i =0;i<tabcontent.length;i++){
+      tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for(i = 0; i < tablinks.length;i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(subjectName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
